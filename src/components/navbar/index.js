37807cf2b7240c503befc31fba07754dd9495405
GuilhermeSeveriano <guiseveriano.10@gmail.com>
2022-02-15 17:00:52 -0300

    Navbar OK

---
+++ src/components/navbar/index.js
@@ -0,0 +1,21 @@
+import "./style.css";
+import navSenai from "../../assets/img/logos_senai_preto.png";
+import bandeira from "../../assets/img/bandeira.png";
+const Navbar = () => {
+  return (
+    <navbar className="cabecalho mt-auto py-2 bg-dark d-flex">
+      "
+      <div className="container organizacao">
+        <div className="logo">
+          <img src={navSenai} alt="logo Senai"></img>
+        </div>
+        <div className="logo2 text-white">
+          <img src={bandeira} alt="bandeira suiço brasileira"></img>
+          <a href="https://suicobrasileira.sp.senai.br/" rel="noreferrer"target="_blank">/suiçobrasileira </a>
+        </div>
+      </div>
+    </navbar>
+  );
+};
+
+export default Navbar;
