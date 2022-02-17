47ba894fea1be3784f45b2552bb563280e7c826b
GuilhermeSeveriano <guiseveriano.10@gmail.com>
2022-02-16 22:42:28 -0300

    Posicionamento

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,7 @@
 import Footer from "../../components/footer";
 import Navbar from "../../components/navbar";
 import "./style.css";
-import qrcode from "../../assets/img/flowcode.png";
+import qrcode from "../../assets/img/frame.png";
 import sesisenai from "../../assets/img/logo-sesi-senai.jpg";
 const Home = () => {
   return (
@@ -9,20 +9,19 @@ const Home = () => {
       <Navbar />
       <div className="fundo">
         <div className="AreaLogin">
-          <form className="qrcode">
-            <div className="form-group">
-              <div className="imgsesisenai">
-                <img src={sesisenai} alt="logo senai" />
-              </div>
-              <img src={qrcode} alt="QRCode"></img>
-              <p class="link">
-                <a href="#paracadastro">Cadastre-se</a>
-              </p>
+          
+          <div className="Area1">
+            <div className="imgsesisenai">
+              <img src={sesisenai} alt="logo senai" />
             </div>
-          </form>
+            <div className="qrcode">
+              <img src={qrcode} alt="qrcode"/>
+            </div>
+            <div className="cadastre">Escaneie o QRCode</div>
+          </div>
 
           {/* -------Login Comum ---------------------------------------------------------------------------------------------------- */}
-          <div className="LoginComum">
+          <div className="Area">
             <form className="item-form" method="post" action="">
               <div className="imgsesisenai">
                 <img src={sesisenai} alt="logo senai" />
